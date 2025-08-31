289b49e870bb2eda2c58d43e1b1fba7d5bdd984d
rajbain <98248515+rajbain@users.noreply.github.com>
2025-08-31 15:47:54 +0530

    Add files via upload

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,13 @@
+import pytest
+from app import app
 
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
+    assert b"Hello, Flask!" in resp.data
